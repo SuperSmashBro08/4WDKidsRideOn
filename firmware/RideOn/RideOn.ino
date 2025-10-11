a97f14cf19d2d9341a69db26b3403e9f4d10741a
John Michael <michael.wilkes@maddmanufacturing.com>
2025-10-10 23:38:46 -0500

    Create RideOn.ino

---
+++ firmware/RideOn/RideOn.ino
@@ -0,0 +1,12 @@
+const int LPWM = 15;
+const int RPWM = 16;
+
+void setup() {
+  pinMode(LPWM, OUTPUT);
+  pinMode(RPWM, OUTPUT);
+  
+  digitalWrite(LPWM, LOW);   // Low side off
+  digitalWrite(RPWM, HIGH);  // Force high
+}
+
+void loop() {}
