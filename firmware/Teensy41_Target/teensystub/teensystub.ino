492dc4c3bfa36c558e4ebe70a7b876bad680d4b5
John Michael <michael.wilkes@maddmanufacturing.com>
2025-10-26 19:23:47 -0500

    upd

--- firmware/Teensy41_Target/teensystub/teensystub.ino
+++ firmware/Teensy41_Target/teensystub/teensystub.ino
@@ -20,7 +20,7 @@ static constexpr uint8_t  LED_PIN    = 13;
 static constexpr uint32_t BAUD_USB  = 115200;   // Serial (USB) to PC
 static constexpr uint32_t BAUD_UART = 115200;   // Serial2 (pins 7/8) to ESP32
 static constexpr char     OTA_TOKEN[] = "8d81ab8762c545dabe699044766a0b72";
-static constexpr char     FW_VERSION[] = "fw-simple-ota-v2";
+static constexpr char     FW_VERSION[] = "fw-simple-ota-v3";
 static constexpr uint8_t POT_PIN = 27;
 
 static volatile bool handshake_ready = false;
