75639a297f4fb485f680b47714cdfe2550ce8dd6
John Michael <michael.wilkes@maddmanufacturing.com>
2025-10-31 12:41:04 -0500

    update

--- firmware/Teensy41_Target/ota/include/jump_to_app.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <cstdint>
-
-namespace ota
-{
-[[noreturn]] void jump_to_app(std::uint32_t app_base_address);
-}
--- firmware/Teensy41_Target/ota/mock/mock_flash.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cstddef>
-
-namespace ota::mock
-{
-void init_flash(std::size_t size);
-void deinit_flash();
-}
